72bcef8d2f8896333edd5bae9fd3a76d2618bf37
Zarubin Aleksandr <zarubin_a@worksap.co.jp>
2018-08-10 16:39:24 +0900

    lint script

--- update.sh
+++ update.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-if [ -z ${PLUGIN_NAMESPACE} ]; then
+if [ -z ""${PLUGIN_NAMESPACE}"" ]; then
   PLUGIN_NAMESPACE="default"
 fi
 
-if [ -z ${PLUGIN_KUBERNETES_USER} ]; then
+if [ -z "${PLUGIN_KUBERNETES_USER}" ]; then
   PLUGIN_KUBERNETES_USER="default"
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_TOKEN} ]; then
+if [ ! -z "${PLUGIN_KUBERNETES_TOKEN}" ]; then
   KUBERNETES_TOKEN=$PLUGIN_KUBERNETES_TOKEN
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_SERVER} ]; then
+if [ ! -z "${PLUGIN_KUBERNETES_SERVER}" ]; then
   KUBERNETES_SERVER=$PLUGIN_KUBERNETES_SERVER
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
-  KUBERNETES_CERT=${PLUGIN_KUBERNETES_CERT}
+if [ ! -z "${PLUGIN_KUBERNETES_CERT}" ]; then
+  KUBERNETES_CERT="${PLUGIN_KUBERNETES_CERT}"
 fi
 
-kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
-if [ ! -z ${KUBERNETES_CERT} ]; then
-  echo ${KUBERNETES_CERT} | base64 -d > ca.crt
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
+kubectl config set-credentials default --token="${KUBERNETES_TOKEN}"
+if [ ! -z "${KUBERNETES_CERT}" ]; then
+  echo "${KUBERNETES_CERT}" | base64 -d > ca.crt
+  kubectl config set-cluster default --server="${KUBERNETES_SERVER}" --certificate-authority=ca.crt
 else
   echo "WARNING: Using insecure connection to cluster"
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+  kubectl config set-cluster default --server="${KUBERNETES_SERVER}" --insecure-skip-tls-verify=true
 fi
 
-kubectl config set-context default --cluster=default --user=${PLUGIN_KUBERNETES_USER}
+kubectl config set-context default --cluster=default --user="${PLUGIN_KUBERNETES_USER}"
 kubectl config use-context default
 
 # kubectl version
 IFS=',' read -r -a DEPLOYMENTS <<< "${PLUGIN_DEPLOYMENT}"
 IFS=',' read -r -a CONTAINERS <<< "${PLUGIN_CONTAINER}"
-for DEPLOY in ${DEPLOYMENTS[@]}; do
-  echo Deploying to $KUBERNETES_SERVER
-  for CONTAINER in ${CONTAINERS[@]}; do
-    if [[ ${PLUGIN_FORCE} == "true" ]]; then
+for DEPLOY in "${DEPLOYMENTS[@]}"; do
+  echo Deploying to "$KUBERNETES_SERVER"
+  for CONTAINER in "${CONTAINERS[@]}"; do
+    if [[ "${PLUGIN_FORCE}" == "true" ]]; then
       echo "Force rollout by applying meaningless annotation"
-      kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG}
-      kubectl patch deployment -n ${PLUGIN_NAMESPACE} ${DEPLOY} -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedAt\":\"`date +'%s'`\"}}}}}"
+      kubectl -n "${PLUGIN_NAMESPACE}" set image deployment/"${DEPLOY}" "${CONTAINER}"="${PLUGIN_REPO}":"${PLUGIN_TAG}"
+      kubectl patch deployment -n "${PLUGIN_NAMESPACE}" "${DEPLOY}" -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"updatedAt\":\"`date +'%s'`\"}}}}}"
     fi
-    kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG} --record
+    kubectl -n "${PLUGIN_NAMESPACE}" set image deployment/"${DEPLOY}" "${CONTAINER}"="${PLUGIN_REPO}":"${PLUGIN_TAG}" --record
   done
 done
